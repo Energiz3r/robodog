9e3b7bc4017c3fb52c77ce66fe6332e842d6b8fd
Tim Eastwood <tim.eastwood@hotmail.com>
2024-05-01 10:34:41 +1000

    adjust accel

--- servo.js
+++ servo.js
@@ -1,3 +1,28 @@
+var chalk = require('chalk');
+
+let i2cBus;
+let Pca9685Driver;
+
+try {
+    i2cBus = require('i2c-bus');
+    Pca9685Driver = require('pca9685').Pca9685Driver;
+} catch (error) {
+    console.log(chalk.yellow('Hardware-related libraries are not installed. Using dummy classes instead'));
+
+    // This is where you would define or import your dummy classes
+    i2cBus = class {
+        constructor() {
+            console.log('Dummy i2c-bus class');
+        }
+    }
+
+    Pca9685Driver = class {
+        constructor() {
+            console.log('Dummy Pca9685Driver class');
+        }
+    }
+}
+
 class Servo {
 
     angle = 0;
