5aad285176f1c92d182f0faad33bc8295a7a4399
Tim Eastwood <tim.eastwood@hotmail.com>
2024-05-01 17:11:43 +1000

    validate local

--- servoController.js
+++ servoController.js
@@ -32,8 +32,8 @@ class dummyPCA {
 
 try {
     i2cBus = require('i2c-bus');
-    Pca9685DriverTest = require('pca9685').Pca9685Driver;
-    Pca9685Driver = Pca9685DriverClass
+    Pca9685Driver = require('pca9685').Pca9685Driver;
+    //Pca9685Driver = Pca9685DriverClass
     console.log(chalk.green('Hardware libraries OK!'));
 } catch (error) {
     console.log(chalk.yellow('Hardware libraries not available! Simulating servo output.'));
