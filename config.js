c170378d54275bf6aa2569e6390497c7ba38ecc0
Energiz3r <tim.eastwood@hotmail.com>
2024-05-08 20:30:02 +1000

    config

--- config.js
+++ config.js
@@ -4,17 +4,17 @@ const config = {
     loopDelayMs: 25,
     calibrate: false,
     motors: {
-        FR_SHOULDER: {channel: 12, defaultAngle: 60, offset: 0},
-        FR_ELBOW: {channel: 13, defaultAngle: 90, offset: 0},
+        FR_SHOULDER: {channel: 12, defaultAngle: 60, offset: -8},
+        FR_ELBOW: {channel: 13, defaultAngle: 90, offset: 2},
         FR_HIP: {channel: 14, defaultAngle: 90, offset: 0},
-        FL_SHOULDER: {channel: 8, defaultAngle: 120, offset: 0},
-        FL_ELBOW: {channel: 9, defaultAngle: 90, offset: 0},
+        FL_SHOULDER: {channel: 8, defaultAngle: 120, offset: 6},
+        FL_ELBOW: {channel: 9, defaultAngle: 90, offset: -9},
         FL_HIP: {channel: 10, defaultAngle: 90, offset: 0},
         BR_SHOULDER: {channel: 0, defaultAngle: 60, offset: 0},
-        BR_ELBOW: {channel: 1, defaultAngle: 90, offset: 0},
+        BR_ELBOW: {channel: 1, defaultAngle: 90, offset: 10},
         BR_HIP: {channel: 2, defaultAngle: 90, offset: 0}, // unused
-        BL_SHOULDER: {channel: 4, defaultAngle: 120, offset: 0},
-        BL_ELBOW: {channel: 5, defaultAngle: 90, offset: 0},
+        BL_SHOULDER: {channel: 4, defaultAngle: 120, offset: 8},
+        BL_ELBOW: {channel: 5, defaultAngle: 90, offset: -3},
         BL_HIP: {channel: 6, defaultAngle: 90, offset: 0} // unused
     },
     pca9685: {
